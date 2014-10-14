01a75b4a656fd68fc69752f849116237e8778a28
relotnek <ken@kentoler.com>
2014-10-14 17:01:59 -0400

    changed the democonstantize method

--- metapoc/app/controllers/demos_controller.rb
+++ metapoc/app/controllers/demos_controller.rb
@@ -21,6 +21,20 @@ class DemosController < ApplicationController
 	def democonstantize
 	end
 
+	def democonstantizecreate
+		@payment_method = params[:payment_method].delete(:type).constantize.new(payment_method_params)
+        @object = @payment_method
+        invoke_callbacks(:create, :before)
+        if @payment_method.save
+          invoke_callbacks(:create, :after)
+          flash[:success] = Spree.t(:successfully_created, :resource => Spree.t(:payment_method))
+          redirect_to edit_admin_payment_method_path(@payment_method)
+        else
+          invoke_callbacks(:create, :fails)
+          respond_with(@payment_method)
+        end
+	end
+
 	def demoeval
 	end
 
