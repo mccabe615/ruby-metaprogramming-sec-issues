ba889af55896c941626d80f8ef750cbfb4c2d572
Mike McCabe <mccabe615@gmail.com>
2014-10-23 22:56:54 -0500

    fixing eval example

--- codecamp/sinny.rb
+++ codecamp/sinny.rb
@@ -1,15 +1,13 @@
 require 'sinatra'
 require 'time'
-require 'securerandom'
-require 'haml'
 
 class Sinny < Sinatra::Base
-  enable :sessions
-  set :session_secret, "My session secret"
 
   get '/' do
-    x = eval params[:code].to_s
-    render x
+    @result=eval(params[:code])
+    "Results from Ruby command: "
+    erb :results
+    
   end
 
 end
